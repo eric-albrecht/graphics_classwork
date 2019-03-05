c7dd2e714e5903e813a87cdcdd8213afb33214e5
albrecht3era <albrechter@msoe.edu>
2019-03-05 17:47:57 -0600

    adds constant sources for the stl reading

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -4,8 +4,12 @@
 
 using namespace std;
 
-int main(){
-	cout << "Hello World!" << endl;
-	read_next_stl("eric.stl");
+int main(int argc, char *argv[])
+{
+	if(argc < 2)
+	{
+		cout << "STL File to read is not listed" << endl;
+	}
+	read_next_stl(argv[1]);
 	return 0;
 }

38377573759af031b431bebdaad768b3f3e97f9f
eric-albrecht <eric.r.albrecht@gmail.com>
2019-03-05 16:51:04 -0600

    adds read in

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "readstl.h"
+#include <string>
 
 using namespace std;
 
 int main(){
 	cout << "Hello World!" << endl;
-	read_next_stl();
+	read_next_stl("eric.stl");
 	return 0;
 }
--- lab1/src/readstl.cpp
+++ lab1/src/readstl.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int read_next_stl() 
+int read_next_stl(string filename)
 {
-	cout << "This is the readstl utility!" << endl;
+	ifstream input(filename.c_str());
+	string linetoread;
+	while(!input.eof()) {
+		getline(input, linetoread);
+		cout << linetoread << endl;
+	}
 	return 0;
 }
